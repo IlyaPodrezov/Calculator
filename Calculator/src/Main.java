f8076f946c1d613ce05e91bc79a526d70853e3d2
IlyaPodrezov <128175496+IlyaPodrezov@users.noreply.github.com>
2023-03-17 19:18:15 +0200

    Add files via upload

---
+++ Calculator/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Выберите операцию (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Некорректная операция");
+                return;
+        }
+
+        System.out.println("Результат: " + result);
+    }
+}
